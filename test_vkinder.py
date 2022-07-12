a0b75571b5a99951acbe884f649d5641a36434a3
Nikolay Kotov <98932850+NikKotov95@users.noreply.github.com>
2022-07-12 23:09:10 +1000

    Update test_vkinder.py

--- test_vkinder.py
+++ test_vkinder.py
@@ -1,6 +1,7 @@
-import  pytest
+import pytest
 from vk_functions import search_users, get_photo, sort_likes, write_msg, add_user, add_user_photos, \
     add_to_black_list
+
 
 class TestVkinder:
 
@@ -18,18 +19,18 @@ class TestVkinder:
     # Проверка поиска анкет
     @pytest.mark.parametrize('sex, age_at, age_to, city, result', [
         ('1', '18', '21', 'Москва', True)])
-    def test_get_photo(self, user_id, result):
-        assert get_photo(user_id) == result
+    def test_search_users(self, sex, age_at, age_to, city, result):
+        assert search_users(sex, age_at, age_to, city) == result
 
     # Тест поиска фотографии
-    @pytest.mark.parametrize('user_id, result', [('336261034'), True])
+    @pytest.mark.parametrize('user_id, result', [('336261034', True)])
     def test_get_photo(self, user_id, result):
         assert get_photo(user_id) == result
 
     # Тест сортировки по лайкам
     @pytest.mark.parametrize('list_photos, result',
                              [(['1', 'photo_1', '2', 'photo_2', 'photo_3'],
-                               ['1', '2', '3', 'photo_1', 'photo_2', 'photo_3']),])
+                               ['1', '2', '3', 'photo_1', 'photo_2', 'photo_3'])])
     def test_sort_likes(self, list_photos, result):
         assert sort_likes(list_photos) == result
 
@@ -47,16 +48,19 @@ class TestVkinder:
         assert add_user(event_id, vk_id, first_name, second_name, city, link, id_user) == result
 
     # Тест добавления фото анкеты в БД
-    @pytest.mark.parametrize('event_id, link_photo, count_likes, id_dating_user, result'
+    @pytest.mark.parametrize('event_id, link_photo, count_likes, id_dating_user, result',
                              [('123', 'Link_link', '2', '3350252', False)])
     def test_add_user_photos(self, event_id, link_photo, count_likes, id_dating_user, result):
         assert add_user_photos(event_id, link_photo, count_likes, id_dating_user) == result
 
     # Добавление в черный список
-    @pytest.mark.parametrize('event_id, vk_id, first_name, second_name, city, link, link_photo, count_likes, id_user, result',
-                             [('123', '12', '12434', '1251321', 'sdfsdfs', 'fsdfsdfs', '12', '123', False)])
-    def test_add_user_to_black_list(self, event_id, vk_id, first_name, second_name, city, link, link_photo, count_likes, id_user, result):
-        assert add_to_black_list(event_id, vk_id, first_name, second_name, city, link, link_photo, count_likes, id_user) == result
+    @pytest.mark.parametrize(
+        'event_id, vk_id, first_name, second_name, city, link, link_photo, count_likes, id_user, result',
+        [('123', '12', '12434', '1251321', 'sdfsdfs', 'fsdfsdfs', '12', '123', False)])
+    def test_add_user_to_black_list(self, event_id, vk_id, first_name, second_name, city, link, link_photo, count_likes,
+                                    id_user, result):
+        assert add_to_black_list(event_id, vk_id, first_name, second_name, city, link, link_photo, count_likes,
+                                 id_user) == result
 
     def teardow_class(self):
         print('method teardown_class')
